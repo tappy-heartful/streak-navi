c2822f80424cc75bb0fc2199b16611ce51474508
MellowZtmy <tappynanakoro@gmail.com>
2025-07-24 14:26:32 +0900

    ログイン実装中

--- app/login/login.js
+++ app/login/login.js
@@ -1,6 +1,35 @@
+////////////////////////////
+// グローバル変数
+////////////////////////////
+let appsettings = [];
+
+////////////////////////////
+// 初期表示
+////////////////////////////
+$(document).ready(async function () {
+  try {
+    // 設定ファイルを読み込み
+    appsettings = await getJsonData('../../appsettings.json');
+
+    // セッションストレージに保存
+    clearAllAppSession(); // 一度全削除
+    Object.keys(appsettings).forEach((key) => {
+      setSession(key, appsettings[key]);
+    });
+    setSession('isLogin', false);
+    setSession('isTest', false);
+  } catch (error) {
+    // データ読み込み失敗時にエラー表示
+    showError('Failed:', error);
+  }
+});
+
+////////////////////////////
+// ログインボタン押下
+////////////////////////////
 function loginWithLINE() {
   const clientId = '2007808275';
-  const redirectUri = encodeURIComponent('../app/dashboard/dashboard.html'); // あなたのコールバックURLに変更
+  const redirectUri = encodeURIComponent('../app/dashboard/dashboard.html');
   const state = Math.random().toString(36).substring(2); // 任意の文字列（CSRF対策）
   const scope = 'openid profile email';
 
