134bd76e57573d0310fbe473d46e905af906cbfd
MellowZtmy <tappynanakoro@gmail.com>
2025-07-25 16:26:08 +0900

    投票一覧画面 モック作成

--- app/vote-list/vote-list.js
+++ app/vote-list/vote-list.js
@@ -5,3 +5,23 @@ $(document).ready(async function () {
   // ログイン済みチェック
   checkLogin();
 });
+
+////////////////////////////
+// タブ切り替え処理
+////////////////////////////
+document.querySelectorAll('.tab').forEach((button) => {
+  button.addEventListener('click', () => {
+    document
+      .querySelectorAll('.tab')
+      .forEach((b) => b.classList.remove('active'));
+    document
+      .querySelectorAll('[data-tab-content]')
+      .forEach((c) => c.classList.add('hidden'));
+
+    button.classList.add('active');
+    const target = button.dataset.tab;
+    document
+      .querySelector(`[data-tab-content="${target}"]`)
+      .classList.remove('hidden');
+  });
+});
