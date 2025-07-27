54c714df876595e5fd3ecc3e49830c22206aaf98
MellowZtmy <tappynanakoro@gmail.com>
2025-07-27 22:12:08 +0900

    ユーザ一覧画面 作成

--- app/user-list/user-list.js
+++ app/user-list/user-list.js
@@ -4,24 +4,31 @@
 $(document).ready(async function () {
   // 初期処理
   initDisplay();
-});
+  const users = [
+    {
+      name: 'カウント 太郎',
+      pictureUrl: 'https://profile.line-scdn.net/0h123456789abcdef',
+    },
+    {
+      name: 'サミー 花子',
+      pictureUrl: 'https://profile.line-scdn.net/0habcdef123456789',
+    },
+    {
+      name: 'カウント 一郎',
+      pictureUrl: 'https://profile.line-scdn.net/0hzyx987654321000',
+    },
+  ];
 
-////////////////////////////
-// タブ切り替え処理
-////////////////////////////
-document.querySelectorAll('.tab').forEach((button) => {
-  button.addEventListener('click', () => {
-    document
-      .querySelectorAll('.tab')
-      .forEach((b) => b.classList.remove('active'));
-    document
-      .querySelectorAll('[data-tab-content]')
-      .forEach((c) => c.classList.add('hidden'));
+  const $list = $('#user-list');
+  $list.empty();
 
-    button.classList.add('active');
-    const target = button.dataset.tab;
-    document
-      .querySelector(`[data-tab-content="${target}"]`)
-      .classList.remove('hidden');
+  users.forEach((user) => {
+    const li = $(`
+      <li>
+        <img src="${user.pictureUrl}" alt="${user.name}のアイコン">
+        <span class="username">${user.name}</span>
+      </li>
+    `);
+    $list.append(li);
   });
 });
