fecafd361b7f725aa2d6aa5be77b68dea6420691
MellowZtmy <tappynanakoro@gmail.com>
2025-07-27 22:27:11 +0900

    ユーザー一覧画面 不足していた機能を追加

--- app/user-list/user-list.js
+++ app/user-list/user-list.js
@@ -24,7 +24,7 @@ $(document).ready(async function () {
 
   users.forEach((user) => {
     const li = $(`
-      <li class="user-item">
+      <li class="user-item" onclick="window.location.href = '../user-edit/user-edit.html'">
         <img src="${user.pictureUrl}" alt="${user.name}のアイコン">
         <span class="username">${user.name}</span>
       </li>
