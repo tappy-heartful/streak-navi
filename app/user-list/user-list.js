d3b34fb00bcf77d082066d50410f40ce39613a25
MellowZtmy <tappynanakoro@gmail.com>
2025-07-27 22:07:20 +0900

    ユーザ一覧画面 ひな形作成

---
+++ app/user-list/user-list.js
@@ -0,0 +1,27 @@
+////////////////////////////
+// 初期表示
+////////////////////////////
+$(document).ready(async function () {
+  // 初期処理
+  initDisplay();
+});
+
+////////////////////////////
+// タブ切り替え処理
+////////////////////////////
+document.querySelectorAll('.tab').forEach((button) => {
+  button.addEventListener('click', () => {
+    document
+      .querySelectorAll('.tab')
+      .forEach((b) => b.classList.remove('active'));
+    document
+      .querySelectorAll('[data-tab-content]')
+      .forEach((c) => c.classList.add('hidden'));
+
+    button.classList.add('active');
+    const target = button.dataset.tab;
+    document
+      .querySelector(`[data-tab-content="${target}"]`)
+      .classList.remove('hidden');
+  });
+});
