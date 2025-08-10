5bd6cb22da6a54b7ec2561656c8f694ecd547dcc
MellowZtmy <tappynanakoro@gmail.com>
2025-08-10 11:15:21 +0900

    未使用の関数削除

--- public/app/common/functions.js
+++ public/app/common/functions.js
@@ -194,15 +194,3 @@ export function initDisplay() {
     loadComponent('dialog');
   }
 }
-
-// ハッシュ化
-export async function getHash(lineId) {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(lineId);
-  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashHex = hashArray
-    .map((b) => b.toString(16).padStart(2, '0'))
-    .join('');
-  return hashHex; // ← これを保存用IDに
-}
