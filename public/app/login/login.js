22459b43e6934b265b5a273f682a4448d2260088
MellowZtmy <tappynanakoro@gmail.com>
2025-08-17 01:03:59 +0900

    ログイン周り 不要なtry catch削除、ログ文言修正

--- public/app/login/login.js
+++ public/app/login/login.js
@@ -4,46 +4,26 @@ import * as utils from '../common/functions.js';
 // 初期表示
 ////////////////////////////
 $(document).ready(async function () {
-  try {
-    // セッションストレージ一度全削除
-    utils.clearAllAppSession();
-  } catch (e) {
-    // ログ登録
-    await utils.writeLog({
-      dataId: 'none',
-      action: '初期表示',
-      status: 'error',
-      errorDetail: { message: e.message, stack: e.stack },
-    });
-  }
+  // セッションストレージ一度全削除
+  utils.clearAllAppSession();
 });
 
 ////////////////////////////
 // ログインボタン押下
 ////////////////////////////
 $('#login').click(async function () {
-  try {
-    const redirectUri = encodeURIComponent(
-      utils.globalBaseUrl + '/app/login/callback.html' // テスト環境の場合、今のベースURLに結合
-    );
-    const state = generateAndStoreState();
-    const scope = 'openid profile';
+  const redirectUri = encodeURIComponent(
+    utils.globalBaseUrl + '/app/login/callback.html' // テスト環境の場合、今のベースURLに結合
+  );
+  const state = generateAndStoreState();
+  const scope = 'openid profile';
 
-    const loginUrl =
-      `https://access.line.me/oauth2/v2.1/authorize?` +
-      `response_type=code&client_id=${utils.globalClientId}&redirect_uri=${redirectUri}` +
-      `&state=${state}&scope=${scope}`;
+  const loginUrl =
+    `https://access.line.me/oauth2/v2.1/authorize?` +
+    `response_type=code&client_id=${utils.globalClientId}&redirect_uri=${redirectUri}` +
+    `&state=${state}&scope=${scope}`;
 
-    window.location.href = loginUrl;
-  } catch (e) {
-    // ログ登録
-    await utils.writeLog({
-      dataId: 'none',
-      action: 'ログインボタン押下',
-      status: 'error',
-      errorDetail: { message: e.message, stack: e.stack },
-    });
-  }
+  window.location.href = loginUrl;
 });
 
 // 認証開始直前に呼ぶ
