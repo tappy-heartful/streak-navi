f67ce4d2208a1107fa8e6b53ac638653dc5c3cc5
MellowZtmy <tappynanakoro@gmail.com>
2025-09-08 22:31:26 +0900

    投票確認 youtube角丸

--- public/app/vote-confirm/vote-confirm.js
+++ public/app/vote-confirm/vote-confirm.js
@@ -383,15 +383,31 @@ function setupEventHandlers(voteId, isAdmin, isOpen, uid) {
     e.preventDefault();
     const videoId = $(this).data('video-id');
     const name = $(this).text();
+
     const iframeHtml = `
-    <div style="position:relative; padding-bottom:56.25%; height:0; overflow:hidden; max-width:100%;">
-      <iframe 
+    <div style="
+        position:relative;
+        padding-bottom:56.25%;
+        height:0;
+        overflow:hidden;
+        max-width:100%;
+        border-radius:16px;
+    ">
+      <iframe
         src="https://www.youtube.com/embed/${videoId}?autoplay=1"
         frameborder="0"
         allowfullscreen
-        style="position:absolute; top:0; left:0; width:100%; height:100%;">
+        style="
+          position:absolute;
+          top:0;
+          left:0;
+          width:100%;
+          height:100%;
+          border-radius:16px;
+        ">
       </iframe>
     </div>`;
+
     await utils.showModal(name, iframeHtml);
   });
 
