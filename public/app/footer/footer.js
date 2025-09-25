0a7e7352c865bdadfe0a3867082bb928e2fff867
MellowZtmy <tappynanakoro@gmail.com>
2025-09-25 18:06:33 +0900

    ページシェアボタン追加

--- public/app/footer/footer.js
+++ public/app/footer/footer.js
@@ -2,4 +2,20 @@ import * as utils from '../common/functions.js';
 ////////////////////////////
 // 初期表示
 ////////////////////////////
-$(document).ready(async function () {});
+$(document).ready(async function () {
+  document.getElementById('share-button').addEventListener('click', () => {
+    const url = window.location.href;
+    if (navigator.share) {
+      navigator
+        .share({
+          title: document.title,
+          url: url,
+        })
+        .catch(console.error);
+    } else {
+      navigator.clipboard.writeText(url).then(() => {
+        alert('URLをコピーしました');
+      });
+    }
+  });
+});
