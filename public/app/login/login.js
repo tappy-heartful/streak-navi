a3729cee487974289c915b68c89206d660488513
MellowZtmy <tappynanakoro@gmail.com>
2025-10-23 21:53:02 +0900

    Revert "githubactionsのお試し"
    This reverts commit 34747049abd678ab0ba3861699d6fb1ce73d1109.

--- public/app/login/login.js
+++ public/app/login/login.js
@@ -15,8 +15,6 @@ $(document).ready(async function () {
     $('#title').html(
       '<h1>Streak <span style="color: rgb(208, 2, 2)">T</span>est</h1>'
     );
-  } else {
-    $('#title').append('test');
   }
 
   // URLにcode/stateが付いていたら → LINEログイン処理を開始
